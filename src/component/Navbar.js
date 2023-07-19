30f91514e41573f8e793347a5652582b24fc74f1
Uday1004 <usolanki068@gmail.com>
2023-07-19 19:36:47 +0530

    navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,16 +1,79 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            Navbar
+        <div className="container-fluid"  >
+          <a className="navbar-brand" style={{ marginLeft:10}} href="/">
+            shopsyha
           </a>
+
+          <form className="d-flex " role="search"  >
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="button" type="submit">
+              <FontAwesomeIcon icon={faMagnifyingGlass} size="lg" />
+            </button>
+          </form>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0 mx-4">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" href="/">
+                  Shop
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" href="/">
+                  Exclusive
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link " href="/">
+                  <FontAwesomeIcon icon={faShoppingCart} />
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link active dropdown-toggle lg"
+                  href="/"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <FontAwesomeIcon icon={faUser} size="lg" />
+                </a>
+                <ul className="dropdown-menu" style={{ left: "-70px" }}>
+                  <li>
+                    <a className="dropdown-item" href="/">
+                      Sing-in/Sign-up
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="/">
+                      Details
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="/"></a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
           <button
             className="navbar-toggler"
             type="button"
@@ -22,69 +85,6 @@ export default function Navbar() {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent" >
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  Shop
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  Features
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                <FontAwesomeIcon icon={faShoppingCart} />
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="/"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Account
-                </a>
-                <ul className="dropdown-menu" style={{ left: "-70px" }}>
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Something{" "}
-                    </a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </div>
         </div>
       </nav>
     </>
